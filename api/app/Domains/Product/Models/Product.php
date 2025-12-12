3cbf42ccae99e6616a982f2f6bec7590ed9f1c9d
Matt Brown <22159690+matthewbrown1234@users.noreply.github.com>
2025-12-12 13:14:54 -0700

    testing commit hook

--- api/app/Domains/Product/Models/Product.php
+++ api/app/Domains/Product/Models/Product.php
@@ -11,7 +11,6 @@ use Illuminate\Support\Carbon;
 
 /**
  * @property string $id
- * @property string $name
  * @property string|null $description
  * @property int $price
  * @property Carbon|null $created_at
