7cde9c2493217eaa0f0cb06ad9e664285354948e
Matt Brown <22159690+matthewbrown1234@users.noreply.github.com>
2025-12-18 18:39:12 +0000

    Adding actual formatting of currency

--- api/app/Domains/Product/Http/Resources/ProductResource.php
+++ api/app/Domains/Product/Http/Resources/ProductResource.php
@@ -5,6 +5,7 @@ namespace App\Domains\Product\Http\Resources;
 use App\Domains\Product\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use NumberFormatter;
 
 /**
  * @mixin Product
@@ -14,11 +15,15 @@ class ProductResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
+        $numberFormatter = new \NumberFormatter("en_US", \NumberFormatter::CURRENCY);
+        $numberFormatter->setAttribute(NumberFormatter::ROUNDING_MODE, NumberFormatter::ROUND_HALFEVEN);
         return [
             'id' => $this->id,
             'name' => $this->name,
             'description' => $this->description,
             'price' => $this->price,
+            'formattedPrice' => $numberFormatter
+                ->formatCurrency($this->price / 10000, "USD"),
             'createdAt' => $this->created_at,
             'updatedAt' => $this->updated_at,
         ];
